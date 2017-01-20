a2f5e6d5be2ef373f7d821eaca74f60a0ac70454
SteamPoweredAnimal <dot@steamfox.net>
2017-01-20 23:28:10 +0100

    Bez idea

--- Assets/scripts/Structures/LevelData.cs
+++ Assets/scripts/Structures/LevelData.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Security.Cryptography;
+using UnityEngine;
 
 namespace Structures
 {
 	[Serializable]
 	public class LevelData
 	{
+		public const int Width = 16;
+		public const int Height = 9;
+
 		public string[] RoomNames;
 		public int[,] Floor;
 
+		public void Generate(GameObject parent)
+		{
+			for(int i = 0; i < Width; i++)
+			{
+				for(int j = 0; j < Height; j++)
+				{ // Generate room
+				}
+			}
+		}
 	}
 }
