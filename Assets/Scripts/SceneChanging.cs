e0346c546c041f2b88ded35a43a85094e8faec05
lukasz <lukasz.stryjek@gmail.com>
2017-01-22 16:11:12 +0100

    start scene, fading and changind to Main

---
+++ Assets/Scripts/SceneChanging.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanging : MonoBehaviour {
+	//public float changeDelay = 1.0;
+
+
+	private IEnumerator SceneChange(float delay)
+	{
+		float fadeTime = GameObject.Find ("sceneSwitcher").GetComponent<SceneFading> ().BeginFade (1);
+		yield return new WaitForSeconds (fadeTime);
+		Application.LoadLevel ("Main");
+	}
+
+	void OnGUI()
+	{
+		if (Input.anyKey) {
+			StartCoroutine (SceneChange (1f));
+
+		}
+	}
+}
