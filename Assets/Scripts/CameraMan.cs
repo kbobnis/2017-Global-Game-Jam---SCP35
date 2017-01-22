3e7a0dcac9b9e98b8e0371f8e6242592a6c5ff70
SteamPoweredAnimal <dot@steamfox.net>
2017-01-22 17:41:14 +0100

    Chase

--- Assets/Scripts/CameraMan.cs
+++ Assets/Scripts/CameraMan.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Controllers;
+using UnityEngine;
 
 public class CameraMan : MonoBehaviour {
 
@@ -13,5 +14,6 @@ public class CameraMan : MonoBehaviour {
 
 	internal void Follow(Transform transform) {
 		Following = transform;
+		AIController.Target = transform;
 	}
 }
